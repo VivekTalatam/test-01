8c5fbb255fd64d041fd0e077defc258a9081701e
Vivek Talatam <vivektalatam2000@gmail.com>
2024-11-15 17:15:16 +0530

    adding sample.sh

--- sample.sh
+++ sample.sh
@@ -3,4 +3,6 @@ name1=$1
 name2=$2
 planet=$3
 script_name=$0
-echo "Hello, $name1 & $name2 $planet running from $script_name $4 :)"
+date=$(date)
+list=$(ls -lrat)
+echo "Hello, $name1 & $name2 $planet running from $script_name $date $list"
