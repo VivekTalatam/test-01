4dc08d58c29145d7f7ddbc859fd360f80ffa24e2
Vivek Talatam <vivektalatam2000@gmail.com>
2024-11-15 17:04:39 +0530

    adding sample.sh

--- sample.sh
+++ sample.sh
@@ -3,4 +3,4 @@ name1=$1
 name2=$2
 planet=$3
 script_name=$0
-echo "Hello, $name1 & $name2 $planet running from $script_name :)"
+echo "Hello, $name1 & $name2 $planet running from $script_name $4 :)"
